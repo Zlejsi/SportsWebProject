77666acba32db3965b2eb85a5dd3c8ab034eaeee
Zlejsi <michal.zlacky@seznam.cz>
2018-07-27 08:02:36 +0200

    add flask

--- SportsWebPython/main.py
+++ SportsWebPython/main.py
@@ -1,9 +1,16 @@
 """
 This script runs the SportsWebPython application using a development server.
 """
-
+import sys
 from os import environ
 from SportsWebPython import app
+import google
+
+#gae_dir = google.__path__.append('C:\Program Files (x86)\Google\google_appengine')
+#sys.path.insert(0, google.__path__[1]) # might not be necessary
+
+#import google.appengine # now it's on your import path`
+from google.appengine.ext.webapp.util import run_wsgi_app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -11,4 +18,5 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    #app.run(HOST, PORT)
+    run_wsgi_app(app)
